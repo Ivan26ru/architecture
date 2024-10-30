5fbffbe2715b74354bde45d998ec43485e23738c
Ivan <it-1ivan@ya.ru>
2024-10-30 23:39:41 +0500

    hw2 wip

--- app/tests/Unit/hw2Test.php
+++ app/tests/Unit/hw2Test.php
@@ -13,7 +13,7 @@ class hw2Test extends TestCase
      * Для объекта, находящегося в точке (12, 5) и движущегося со скоростью (-7, 3) движение меняет положение объекта на (5, 8)
      * @return void
      */
-    public function test_hw2()
+    public function test_hw2_1()
     {
         $starShip  = new UObject();
         $movingObj = new MovingObjectAdapter($starShip);
@@ -30,5 +30,57 @@ class hw2Test extends TestCase
             'Тест не прошел: для объекта, находящегося в точке (12, 5) и движущегося со скоростью (-7, 3)
      * движение меняет положение объекта на (5, 8);');
 
+    }
+
+    /**
+     * Попытка сдвинуть объект, у которого невозможно прочитать положение в пространстве, приводит к ошибке
+     * @return void
+     */
+    public function test_hw2_2_error()
+    {
+        $starShip  = new UObject();
+        $movingObj = new MovingObjectAdapter($starShip);
+
+        $starShip->setMapping('Location', null);
+        $starShip->setMapping('Velocity', new Vector(-7, 3));
+
+        $movingCommand = new MoveCommand($movingObj);
+        $this->expectException(Throwable::class);
+
+        $movingCommand->execute();
+    }
+
+    /**
+     * Попытка сдвинуть объект, у которого невозможно прочитать значение мгновенной скорости, приводит к ошибке
+     * @return void
+     */
+    public function test_hw2_3_error()
+    {
+        $starShip  = new UObject();
+        $movingObj = new MovingObjectAdapter($starShip);
+
+//        $starShip->setMapping('Location', null);
+//        $starShip->setMapping('Velocity', new Vector(-7, 3));
+
+        $movingCommand = new MoveCommand($movingObj);
+        $this->expectException(Throwable::class);
+
+        $movingCommand->execute();
+    }
+    /**
+     * Попытка сдвинуть объект, у которого невозможно прочитать значение мгновенной скорости, приводит к ошибке
+     * @return void
+     */
+    public function test_hw2_4_error()
+    {
+        $starShip  = new UObject();
+        $movingObj = new MovingObjectAdapter($starShip);
+
+        $starShip->setMapping('Location', new Vector("33", 3));
+
+        $movingCommand = new MoveCommand($movingObj);
+        $this->expectException(Throwable::class);
+
+        $movingCommand->execute();
     }
 }
