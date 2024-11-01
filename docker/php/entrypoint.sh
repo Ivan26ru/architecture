107665ad76643bf30f5263e5171fc79f22aae7d6
Иван Писаренко <pisarenko@arnica.pro>
2024-11-01 11:49:26 +0500

    hw3 fix docker

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -2,9 +2,4 @@
 #выполняется при создании образа
 composer install
 
-# Нужен для laravel
-sudo chown -R www-data:www-data /var/www/storage
-sudo chown -R www-data:www-data /var/www/bootstrap/cache
-
-php artisan key:generate
 exec "${@-php-fpm}"
